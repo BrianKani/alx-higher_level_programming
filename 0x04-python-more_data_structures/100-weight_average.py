e689350a98aab4977ce1b4e8ceccb2c0f4c2eccb
BrianKani <kipsangkangogo26@gmail.com>
2024-03-22 09:55:16 +0000

    weighted average of all integers tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+ght_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
